aec9ae8064d1ec78f2c2cc27e192bfaee1be4145
Pramish Luitel <pramish.luitel7@gmail.com>
2020-07-19 15:14:08 +1000

    Sum of a number taken from user input

--- SimpleArraySum/README.md
+++ SimpleArraySum/README.md
@@ -1,3 +1,5 @@
 ## Question -> Given an array of integers, find the sum of its elements.
 
-For example: [1,2,3] -> 1+2+3 -> 6
+For example: [1,2,3] -> 1+2+3-> 6
+
+[Question to Sum of an Array](https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true)
--- SimpleArraySum/SimpleArraySum.js
+++ SimpleArraySum/SimpleArraySum.js
@@ -1,9 +1,31 @@
-function FindSum() {
-  let sum = 0;
-  const array = [31, 4, 1, 5, 1, 2, 1, 7, 1, 8, 1, 9, 1, 1, 2, 1, 85];
-  array.forEach((value) => {
-    sum = sum + value;
-  });
-  console.log(sum);
-}
-FindSum();
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let size;
+let array = [];
+let sum = 0;
+rl.question('Enter the size of an array: ', (answer) => {
+  size = answer;
+  rl.question(
+    `Enter the element for an array size ${size} separated by a space: `,
+    (answer) => {
+      let number = answer.split(' ');
+      number.forEach((value) => {
+        array.push(parseInt(value));
+      });
+      if (array.length > size) {
+        throw new Error(
+          `Please enter the valid array size you've defined i.e. ${size}`
+        );
+      } else {
+        array.forEach((value) => {
+          sum = sum + value;
+        });
+        console.log(`The sum of ${answer} is ${sum}`);
+      }
+      rl.close();
+    }
+  );
+});
